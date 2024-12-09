edfdf2648f1ebd7ac94463c7b3a05e6e044722c1
joalopez <joalopez@ug.uchile.cl>
2024-12-09 15:42:03 -0300

    Refactor WORD_DICT creation and enhance type handling in to_json function

--- ejemplo.py
+++ ejemplo.py
@@ -16,7 +16,7 @@ with open("datos_12.json", "r") as f:
 
 with open("palabras_12.csv", "r") as f:
     lls = [l.split(",") for l in f.readlines()[1:]]
-    WORD_DICT = {l[0]:l[1] for l in lls}
+    WORD_DICT = {l[0].strip():int(l[1].strip()) for l in lls}
 
 def get_distance_one(tx, word):
     QUERY = """match (a:Word)-[r:has_syn]->(b:Word) where a.palabra = $word return b"""
@@ -33,30 +33,37 @@ def get_distance_one(tx, word):
     #records, _, _ = driver.execute_query(QUERY, routing_=RoutingControl.READ, database_="neo4j")
     return [n for n in nodes], [r for r in result] 
 
-def to_json(word, wordid, records, nodos_a_agregar):
+def to_json(word: str, wordid: int, records, nodos_a_agregar):
     retdict = dict()
     nodes = set()
     relationships = []
 
     
     nodes.add((wordid, word))
-    nodes_dist_one = {(x['b']['id'], x['b']['palabra']) for x in nodos_a_agregar}
+    nodes_dist_one = {(int(x['b']['id']), x['b']['palabra']) for x in nodos_a_agregar}
     nodes = nodes.union(nodes_dist_one)
     for x in nodes_dist_one:
-        relationships.append(
-            {"from": wordid,
-                    "to": x[0],
-                    "arrows": {"to": {"enabled": True, "type": "arrow",}},
-                    "color": "rgb(0,0,0)",
-                    })
+        relationships.append({
+                "from": wordid,
+                "to": x[0],
+                "arrows": {"to": {"enabled": True, "type": "arrow",}},
+                "color": "rgb(0,0,0)",
+        })
 
-    for nodo in nodes:
-        records_of_b = [r for r in records if r['b']['id'] == nodo[0] and (r['c']['id'], r['c']['palabra']) in nodes]
+    for iden, word in nodes:
+        records_of_b = [r for r in records 
+                        if int(r['b']['id']) == iden and (int(r['c']['id']), r['c']['palabra']) in nodes]
         for record in records_of_b:
-            relationships.append({"from": record['b']['id'],
-            "to": record['c']['id'],
-            "arrows": {"to": {"enabled": True, "type": "arrow",}},
-            "color": "rgb(0,0,0)"
+            relationships.append({
+                "from": record['b']['id'],
+                "to": record['c']['id'],
+                "arrows": {
+                    "to": {
+                        "enabled": True,
+                        "type": "arrow",
+                    }
+                },
+                "color": "rgb(0,0,0)"
             })
     # colors = [(syn["synonims"],color) for syn,color in zip(DICT[word]["aseptions"], COLORS)]
     # l = []
@@ -89,10 +96,12 @@ def distance_one(word: str) -> dict:
 
 
 def main(query: str):
-    with GraphDatabase.driver(URI, auth=AUTH).session(database="neo4j") as session:
-        _,vecinos = session.read_transaction(get_distance_one, query)
-        for vecino in vecinos:
-            print(f"{vecino['b']['palabra']} -> {vecino['c']['palabra']}")
+    import pprint
+    pprint.pprint(distance_one(query))
+    #with GraphDatabase.driver(URI, auth=AUTH).session(database="neo4j") as session:
+    #    _,vecinos = session.read_transaction(get_distance_one, query)
+    #    for vecino in vecinos:
+    #        print(f"{vecino['b']['palabra']} -> {vecino['c']['palabra']}")
             # path = vecino['p']  # Aquí asumimos que 'p' es el Path
             # inicial_node = path.nodes[0]  # Accede al primer nodo del Path, que es el nodo inicial
             # final_node = path.nodes[-1]  # Accede al último nodo del Path, que es el nodo final
